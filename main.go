5578cbfd653af58c84207e19033a94eaeae29062
ahmadhafis <ahmadhafis296@gmail.com>
2024-11-06 09:14:42 +0700

    add connection to .env

--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
 func main() {
-	
+	// Env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
+
+	// Database
+	db := NewDatabase()
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("failed to get DB from GORM:", err)
+	}
+	defer sqlDB.Close()
+}
+
+func NewDatabase() *gorm.DB {
+	// dsn := "host=147.139.143.164 port=5432 username=batch2_trainee_1 password=nasi_goreng_trainee_1 dbname=batch2_trainee_1 sslmode=disable TimeZone=Asia/Jakarta"
+	dsn := os.Getenv("DATABASE")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("failed to get DB object: %v", err)
+	}
+
+	var currentDB string
+	err = sqlDB.QueryRow("SELECT current_database()").Scan(&currentDB)
+	if err != nil {
+		log.Fatalf("failed to query current database: %v", err)
+	}
+
+	log.Printf("Current Database: %s\n", currentDB)
+
+	return db
 }
